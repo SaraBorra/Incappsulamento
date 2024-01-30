9b938aefdce9e53d2d95aa538d92c0e83cfb7fa4
sarab <borrasara03@gmail.com>
2024-01-30 16:41:22 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Auto miaAuto = new Auto(2000, "XYZ123", "Toyota", "Corolla");
+        System.out.println("Marca: " + miaAuto.getMarca() + ", Modello: " + miaAuto.getModello());
+    }
+}
